548bd3b5ce1058e76da8b52888ddb0318f3e2624
bipaswiii <210406@softwarica.edu.np>
2022-02-02 11:15:52 +0545

    frame

--- main.py
+++ main.py
@@ -4,3 +4,36 @@ from tkinter.font import Font
 import tkinter.messagebox
 from tkinter import filedialog
 import sqlite3
+root=Tk()
+root.title("Bipaswi_Todo List")
+root.geometry("500x800")
+root.resizable(False,False)
+
+    #Font
+my_font=Font(
+    family="Brush Script MT",
+    size=30,
+    weight="bold",)
+
+    #Created frame
+my_frame=Frame(root,)
+my_frame.pack(pady=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mainloop()
+
